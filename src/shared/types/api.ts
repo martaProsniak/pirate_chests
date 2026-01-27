a94363c91a4915057173cb559f33743f76d432e9
Marta Prośnak-Kalwaj <martakalwaj@gmail.com>
2026-01-28 00:05:39 +0100

    Backend: adding api types

--- src/shared/types/api.ts
+++ src/shared/types/api.ts
@@ -3,3 +3,49 @@ export type InitResponse = {
   postId: string;
   username: string;
 };
+
+export type Findings = {
+  chests: number;
+  gold: number;
+  bombs: number;
+}
+
+export type UserStats = {
+  score: number;
+  gamesPlayed: number;
+  wins: number;
+  findings: Findings
+};
+
+export type LeaderboardEntry = {
+  username: string;
+  score: number;
+  rank: number;
+};
+
+export type DailyChallengeResponse = {
+  seed: string;
+  date: string;
+  attempts: number;
+  stats: UserStats;
+  username: string;
+};
+
+export type SubmitScoreRequest = {
+  moves: number;
+  score: number;
+  findings: Findings;
+  isWin: boolean;
+  isDaily: boolean;
+  attempt: number;
+};
+
+export type SubmitScoreResponse = {
+  success: boolean;
+  newStats: UserStats;
+};
+
+export type LeaderboardResponse = {
+  entries: LeaderboardEntry[];
+  userRank?: number;
+};
